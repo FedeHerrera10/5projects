e111ee4b7690aa8d4a39a4603046eed9e80dabad
FedeHerrera10 <herrera3299@gmail.com>
2021-04-02 02:11:24 -0300

    Add slider in header-hero, with only  js and css

--- js/data.js
+++ js/data.js
@@ -2,7 +2,51 @@
 
 const $btnMenu = document.getElementById('btn-menu');
 const $navbar = document.getElementById('navbar');
+const $sliderLeft = document.querySelector('#sliderLeft');
+const $sliderRigth = document.querySelector('#sliderRigth');
+const $container = document.querySelectorAll('.slider');
+
+let currentSlide =1;
+let currentX = 0;
+
 
 $btnMenu.addEventListener('click', () => {
     $navbar.classList.toggle('is-active');
 })
+
+
+$sliderLeft.style.display = 'none';
+
+$sliderRigth.addEventListener('click' ,() => {
+    $sliderLeft.style.display = 'block';
+    if(currentSlide < 2){
+        currentSlide +=1;
+        currentX +=100;
+        translate();
+    }
+
+    if(currentSlide == 2){
+    $sliderRigth.style.display = 'none';
+    }
+})
+
+$sliderLeft.addEventListener('click' ,() => {
+    $sliderRigth.style.display = 'block';
+    
+    if(currentSlide > 0){
+        currentSlide -=1;
+        currentX -=100;
+        translate();
+    }
+
+    if(currentSlide == 1){
+    $sliderLeft.style.display = 'none';
+    }
+    
+})
+
+function translate(){
+    $container.forEach((item) => {
+        item.style.transform=`translateX(-${currentX}%)`;
+    })
+}
